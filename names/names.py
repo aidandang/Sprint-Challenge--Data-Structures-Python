7f552d2e052397f2ea16411ef59c2b7804b1f34b
Aidan Dang <aidandang@gmail.com>
2020-09-25 19:35:42 -0700

    completed names

--- names/names.py
+++ names/names.py
@@ -10,19 +10,77 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+"""
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name_1 in names_1: # O(n)
+    for name_2 in names_2: # O(n) => O(n^2)
+        if name_1 == name_2:  
+            duplicates.append(name_1) # O(n) => O(n * n^2) => O(n^) => quadratic
+"""
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+bst = BSTNode('')
+
+for name in names_1: # O(n)
+    bst.insert(name) # O(1) => O(1) + O(n)
+
+duplicates = []
+
+for name in names_2: # O(n)
+    if bst.contains(name): 
+        duplicates.append(name) # O(n) => O(n) + O(n) => O(2n) => O(n)
+
+# O(n) => linear
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+"""
+duplicates = []
+
+for name in names_1:
+    if names_2.count(name) >= 1:
+        duplicates.append(name)
+"""
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
